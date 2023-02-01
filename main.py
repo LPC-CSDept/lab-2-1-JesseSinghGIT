3644062dc52e1496b4c568c3ef11b9f03f147c02
JesseSinghGIT <123704475+JesseSinghGIT@users.noreply.github.com>
2023-02-01 01:18:44 +0000

    program

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ def main():
    # Comlete your code here
    ##################################################
 
+
+    num1 = int( input( 'What is the regular price of the item? : '))
+    num2 = int (input ('What is the percentage discount on the item? : '))
+    num3 = num2/100
+    num4 = num3* num1
+    num5 = num1 - num4
+    print ('Regular price: ' , num1 ) 
+    print ('Discount amount: ' , num2 ) 
+    print ( 'The final price: ' , num5 ) 
+   
     pass
 
 
